3ac65b8b1e2b9787dd3c9801cb47d58a9e84db6f
giangntse150746 <giangntse150746@fpt.edu.vn>
2021-09-12 13:31:39 +0700

    weekly update

---
+++ js/specific/landing.js
@@ -0,0 +1,175 @@
+const revealHuyenLinh = `
+    <div class="left-side">
+        <img src="source/landing/character-reveal/HuyenLinh.png" alt="reveal-HuyenLinh">
+    </div>
+    <div class="right-side">
+        <h2>
+            Huyên Linh
+        </h2>
+        <div class="bruh-line"></div>
+        <p>
+            Huyên Linh là cựu sinh viên trường đại học Y thành phố K. Cô lanh lợi, sắc bén, luôn sẵn sàng hành động vì công lý và luôn dùng khả năng quan sát nhạy bén của mình để phân tích và đặt câu hỏi cho mọi vấn đề.
+            <br/>
+            Cô có một quá khứ mập mờ. Trong Kỳ Án Ánh Trăng, từng khung hình trôi qua đều là những mảnh ghép giúp ta hiểu rõ hơn về nhân vật này. Để đi tìm câu trả lời cho những thắc mắc còn vương vấn, đích thân cô buộc phải quay
+            trở lại mái trường xưa cũ. Bởi lần này, Huyên Linh đã dặn lòng... “Mình nhất định phải tìm ra lời giải.”
+        </p>
+    </div>
+`;
+
+const revealTranDong = `
+    <div class="left-side">
+        <img src="source/landing/character-reveal/TranDong.png" alt="reveal-TranDong">
+    </div>
+    <div class="right-side">
+        <h2>
+            Trần Đông
+        </h2>
+        <div class="bruh-line"></div>
+        <p>
+            Trần Đông, một thám tử hành nghề lâu năm với vô vàn kinh nghiệm trong việc phá giải những vụ án hóc búa nhất. Anh nghiêm túc, chín chắn, mang phong cách “hard-boiled” của những người duy trì luật pháp và hỗ trợ công lý thẩm sâu trong bóng tối.
+            <br/>
+            Từ lúc Huyên Linh cùng anh tham gia vào vụ án ánh trăng, Trần Đông đóng vai trò như một người bạn đồng hành đáng tin cậy, luôn ở bên vào những lúc cô cần. Với sự hợp tác của cả hai, liệu sự thật có được phơi bày ra ánh sáng?
+        </p>
+    </div>
+`;
+
+const revealAnNhien = `
+    <div class="left-side">
+        <img src="source/landing/character-reveal/TranDong.png" alt="reveal-AnNhien">
+    </div>
+    <div class="right-side">
+        <h2>
+            An Nhiên
+        </h2>
+        <div class="bruh-line"></div>
+        <p>
+            An Nhiên, đúng như cái tên: là một cô bé vô ưu vô lo, có cả một chút ngây ngô của tuổi mới lớn. Cũng bởi vậy, khi phát hiện Huyên Linh là người mới chuyển đến trường nhưng lại học giỏi hơn mình, cô cứ vậy mà giận dỗi những nỗi niềm rất trẻ con. Trong
+            mắt cô, Huyên Linh là đồ đáng ghét, là người trông chẳng xinh đẹp tí nào…
+            <br/>
+            Chuyện gì sẽ đến với An Nhiên? Liệu cô có an toàn trước những biến cố sắp sửa xảy ra? Mối quan hệ giữa cô và Huyên Linh rồi sẽ ra sao? Hay ánh trăng rồi sẽ nuốt chửng tất cả...
+        </p>
+    </div>
+`;
+
+const revealNguoiGia = `
+    <div class="left-side">
+        <img src="source/landing/character-reveal/NguoiGia.png" alt="reveal-NguoiGia">
+    </div>
+    <div class="right-side">
+        <h2>
+            Chủ Nhiệm CLB Nhiếp Ảnh
+        </h2>
+        <div class="bruh-line"></div>
+        <p>
+            Mọi nhân vật chính trong câu chuyện đều cần có một người hỗ trợ xuyên suốt cuộc hành trình. Trong Kỳ Án Ánh Trăng, vai trò đó thuộc về vị chủ nhiệm câu lạc bộ nhiếp ảnh.
+            <br/>
+            Tính khí cô nghiêm túc, có phần hơi khô khan nhưng quan trọng hơn hết là người mà Huyên Linh có thể tin tưởng trông chờ vào. Hai con người không quen biết nhau bao lâu nhưng họ lại đồng lòng chia sẻ chung một chí hướng,
+            liệu cô có phải là chìa khóa mà Huyên Linh cần để sự thật sớm được giải mã?
+        </p>
+    </div>
+`;
+
+const revealTruongLam = `
+    <div class="left-side">
+        <img src="source/landing/character-reveal/TruongLam.png" alt="reveal-TruongLam">
+    </div>
+    <div class="right-side">
+        <h2>
+            Trương Lãm
+            <div hidden>ham lon so 1</div>
+        </h2>
+        <div class="bruh-line"></div>
+        <p>
+            Trương Lãm- giảng viên khoa giải phẫu học của Đại học Y thành phố K, một nhà giáo mẫu mực có trách nhiệm với nghề nghiệp. Là thần tượng của biết bao thế hệ sinh viên trường Y, thầy hòa đồng, vui vẻ, luôn thể hiện sự ân cần mọi lúc mọi nơi.
+            <br/>Trương Lãm còn là một bác sĩ pháp y hỗ trợ tổ trinh sát thành phố K trong các vụ án xảy ra tại Đại học Y này.
+        </p>
+    </div>
+`;
+
+const revealHaVi = `
+    <div class="left-side">
+        <img src="source/landing/character-reveal/HaVi.png" alt="reveal-HaVi">
+    </div>
+    <div class="right-side">
+        <h2>
+            Hạ Vi
+        </h2>
+        <div class="bruh-line"></div>
+        <p>
+            Là một nhân vật với tính cách và nội tâm thú vị, Hạ Vi chính là một trong những nhân vật quan trọng làm nổi bật sự phát triển và tình tiết bộ phim.
+            <br/>Cô là người thân thiện, hòa đồng. Bên cạnh đó Hạ Vi cũng rất hâm mộ và si mê người thầy Trương Lãm của mình. Những góc khuất riêng trong nội tâm đã luôn là 1 đề tài thú vị, và qua hạ Vi, phần nào mọi người sẽ thấy được
+            những điều ấy. Bởi suy cho cùng: ai cũng có phần thiện và phần ác bên trong mình..
+        </p>
+    </div>
+`;
+
+const revealBaoTran = `
+    <div class="left-side">
+        <img src="source/landing/character-reveal/BaoTran.png" alt="reveal-BaoTran">
+    </div>
+    <div class="right-side">
+        <h2>
+            Bảo Trân
+        </h2>
+        <div class="bruh-line"></div>
+        <p>
+            Là đối cực đối với Huyền Khanh. Không chỉ về ngoại hình mà còn cả về tính cách. Bảo Trân có phần thẳng thắn, mạnh mẽ hơn Huyền Khanh. Tuy nhiên họ đều có những điểm chung đến bất ngờ khi cả hai đều thích lo chuyện bao đồng của người khác. Huyền Khanh
+            tung, Bảo Trân hứng. Một hài hước lém lỉnh, một thẳng thắn mỉa mai.
+            <br/>Thế nên Bảo Trân cũng là một nhân vật không kém phần đặc sắc của Kỳ Án Ánh Trăng, góp phần tạo nên những tình tiết bất ngờ.
+        </p>
+    </div>
+`;
+
+const revealHuyenKhanh = `
+    <div class="left-side">
+        <img src="source/landing/character-reveal/HuyenKhanh.png" alt="reveal-HuyenKhanh">
+    </div>
+    <div class="right-side">
+        <h2>
+            Huyền Khanh
+        </h2>
+        <div class="bruh-line"></div>
+        <p>
+            Cùng với 2 người bạn chung ký túc xá Hạ Vi, Bảo Trân: căn phòng 103 cứ như vậy mà trở thành một sân khấu điện ảnh hài kịch điển hình. Huyền Khanh với dáng người mũm mĩm, tính cách cô có phần hóm hỉnh, hài hước. Ngoài ra cô bạn còn có một sở thích: sân
+            si về mọi thứ trong trường. Nơi đâu có chuyện gì, sẽ không bao giờ qua mắt được Huyền Khanh.
+            <br/>Từ đó, cô trở thành một nhân vật thú vị, giảm bớt đi phần nào không khí nặng nề trong một phim trinh thám.
+        </p>
+    </div>
+`;
+
+function reveal(content) {
+    document.getElementById('characterInfo').innerHTML = content;
+    console.log(content);
+}
+
+function triggerReveal(previewContainer) {
+    // get the id's value of the preview-image
+    var previewValue = previewContainer.getElementsByTagName("img")[0].id;
+    console.log(previewValue);
+    switch (previewValue) {
+        case "HuyenLinh":
+            reveal(revealHuyenLinh);
+            break;
+        case "TranDong":
+            reveal(revealTranDong);
+            break;
+        case "AnNhien":
+            reveal(revealAnNhien);
+            break;
+        case "NguoiGia":
+            reveal(revealNguoiGia);
+            break;
+        case "TruongLam":
+            reveal(revealTruongLam);
+            break;
+        case "HaVi":
+            reveal(revealHaVi);
+            break;
+        case "BaoTran":
+            reveal(revealBaoTran);
+            break;
+        case "HuyenKhanh":
+            reveal(revealHuyenKhanh);
+            break;
+    }
+}
